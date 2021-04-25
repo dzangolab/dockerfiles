3bc8e82f32b99a3b02cd7b51bf008c5faa836f88
Olivier Pichon <op@dzangolab.com>
2021-04-25 19:50:27 +0700

    feat(snipe-it): update Dockerfile and start script

--- snipe-it/start.sh
+++ snipe-it/start.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-source /env_secrets_expand.sh
+if [ -n "${APP_KEY_FILE}" ]; then
+  export APP_KEY=$(cat $APP_KEY_FILE)
+fi
+
+if [ -n "$MAIL_ENV_PASSWORD_FILE}" ]; then
+  export MAIL_ENV_PASSWORD=$(cat $MAIL_ENV_PASSWORD_FILE)
+fi
+
+if [ -n "${MYSQL_PASSWORD_FILE}" ]; then
+  export MYSQL_PASSWORD=$(cat $MYSQL_PASSWORD_FILE)
+fi
 
 /startup.sh
