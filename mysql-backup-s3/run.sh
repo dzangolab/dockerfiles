ec23af2b2a2ab2de9521082240dbc9a52717f8d7
Olivier Pichon <op@dzangolab.com>
2021-05-02 11:53:03 +0700

    feat (mysql-backup-s3): updated service to use AWS CLI v2.

--- mysql-backup-s3/run.sh
+++ mysql-backup-s3/run.sh
@@ -2,14 +2,6 @@
 
 set -e
 
-if [ -n "${MYSQL_PASSWORD_FILE}" ]; then
-  export MYSQL_PASSWORD=$(cat $MYSQL_PASSWORD_FILE)
-fi
-
-if [ -n "${S3_SECRET_ACCESS_KEY_FILE}" ]; then
-  export S3_SECRET_ACCESS_KEY=$(cat $S3_SECRET_ACCESS_KEY_FILE)
-fi
-
 if [ "${S3_S3V4}" = "yes" ]; then
     aws configure set default.s3.signature_version s3v4
 fi
