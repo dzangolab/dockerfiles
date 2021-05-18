d30094c6c237fc72777874ca64cd94f77ec499c5
Tilak Poudel <tilak@dzangolab.com>
2021-05-18 18:33:26 +0545

    docs: update readme and add newline at the end of file

--- easyappoinments/README.md
+++ easyappoinments/README.md
@@ -1,12 +1,5 @@
 ### easyappoinments docker file
 
-### Arguments used in Dockerfile
-
-```bash
-DEBIAN_FRONTEND=noninteractive
-EASYAPPOINMENT_RELEASE=1.4.1
-```
-
 ###Installation
 Clone the repository
 
@@ -28,21 +21,19 @@ docker build -t easyappoinment:latest .
 
 ### Usage
 
-Write the `docker-compose.yml` file and add the follwing services.
-
 The app requires `mysql:5.7` image and depends on `mysql`service.
 
 Add the `.env` file with the following variables.
 
 ```bash
-BASE_URL='http://localhost:8000/'
-LANGUAGE='english'
+BASE_URL=http://localhost:8000
+LANGUAGE=english
 DEBUG_MODE=FALSE
 
-DB_HOST='mysql'
-DB_NAME='databasename' // it should match with the environment MYSQL_DATABASE defined in mysql service
-DB_USERNAME='dbusername'
-DB_PASSWORD='dbpassword'
+DB_HOST=mysql
+DB_NAME=databasename // DB_NAME should match with the environment MYSQL_DATABASE defined in mysql service
+DB_USERNAME=dbusername
+DB_PASSWORD=dbpassword
 
 GOOGLE_SYNC_FEATURE=FALSE
 GOOGLE_PRODUCT_NAME=''
--- easyappoinments/config.php
+++ easyappoinments/config.php
@@ -1,29 +1,5 @@
 <?php
-/* ----------------------------------------------------------------------------
- * Easy!Appointments - Open Source Web Scheduler
- *
- * @package     EasyAppointments
- * @author      A.Tselegidis <alextselegidis@gmail.com>
- * @copyright   Copyright (c) 2013 - 2020, Alex Tselegidis
- * @license     http://opensource.org/licenses/GPL-3.0 - GPLv3
- * @link        http://easyappointments.org
- * @since       v1.0.0
- * ---------------------------------------------------------------------------- */
 
-/**
- * Easy!Appointments Configuration File
- *
- * Set your installation BASE_URL * without the trailing slash * and the database
- * credentials in order to connect to the database. You can enable the DEBUG_MODE
- * while developing the application.
- *
- * Set the default language by changing the LANGUAGE constant. For a full list of
- * available languages look at the /application/config/config.php file.
- *
- * IMPORTANT:
- * If you are updating from version 1.0 you will have to create a new "config.php"
- * file because the old "configuration.php" is not used anymore.
- */
 class Config {
 
     // ------------------------------------------------------------------------
@@ -53,6 +29,3 @@ class Config {
     const GOOGLE_CLIENT_SECRET  = '__GOOGLE_CLIENT_SECRET__';
     const GOOGLE_API_KEY        = '__GOOGLE_API_KEY__';
 }
-
-/* End of file config.php */
-/* Location: ./config.php */
