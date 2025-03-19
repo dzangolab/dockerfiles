d962c1743a24c1b122f960867949ebb9fb877631
Anthony Phothirath <anthony.phothirath@hotmail.com>
2025-03-19 13:47:01 +0100

    chore: correcting syntax

--- twentycrm/start.sh
+++ twentycrm/start.sh
@@ -3,7 +3,7 @@
 source /expand_secrets.sh
 
 if [ -z "${PG_DATABASE_URL:-}" ]; then
-  PG_DATABASE_URL=postgres://${PG_DATABASE_USER:-postgres}:${PG_DATABASE_PASSWORD:-postgres}@${PG_DATABASE_HOST:-db}:${PG_DATABASE_PORT:-5432}/default
+  PG_DATABASE_URL=postgres://${PG_DATABASE_USER}:${PG_DATABASE_PASSWORD}@${PG_DATABASE_HOST}:5432/default
   export DATABASE_URL
 fi
 
