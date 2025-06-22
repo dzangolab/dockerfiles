6362dbe11d75b33f413356bcf2de196ea9360257
Olivier Pichon <op@dzango.com>
2025-06-22 16:50:15 +0700

    feat(twentycrm): update to v0.53

--- twentycrm/app/start.sh
+++ twentycrm/app/start.sh
@@ -5,7 +5,4 @@ source /expand_secrets.sh
 PG_DATABASE_URL="postgres://${PG_DATABASE_USER}:${PG_DATABASE_PASSWORD}@${PG_DATABASE_HOST}:5432/${PG_DATABASE_NAME}"
 export PG_DATABASE_URL
 
-yarn database:migrate:prod
-yarn command:prod upgrade
-
 sh /app/entrypoint.sh "$@"
