85690b8f952850fc3b1fccf58e1f7202cf822404
Anthony Phothirath <anthony.phothirath@hotmail.com>
2025-06-25 20:07:57 +0200

    chore: making different folder for postgres backups

---
+++ postgres-16-backup-s3/backup.sh
@@ -0,0 +1,151 @@
+#!/bin/bash
+
+set -x
+set -eo pipefail
+
+echo "Starting backup at $(date)"
+
+if [ -n "${POSTGRES_PASSWORD_FILE}" ]; then
+  POSTGRES_PASSWORD=$(cat "$POSTGRES_PASSWORD_FILE")
+  export POSTGRES_PASSWORD
+fi
+
+if [ -n "${S3_ACCESS_KEY_ID_FILE}" ]; then
+  S3_ACCESS_KEY_ID=$(cat "$S3_ACCESS_KEY_ID_FILE")
+  export S3_ACCESS_KEY_ID
+fi
+
+if [ -n "${S3_SECRET_ACCESS_KEY_FILE}" ]; then
+  S3_SECRET_ACCESS_KEY=$(cat "$S3_SECRET_ACCESS_KEY_FILE")
+  export S3_SECRET_ACCESS_KEY
+fi
+
+if [ "${S3_ACCESS_KEY_ID}" = "**None**" ]; then
+  echo "You need to set the S3_ACCESS_KEY_ID environment variable."
+  exit 1
+fi
+
+if [ "${S3_SECRET_ACCESS_KEY}" = "**None**" ]; then
+  echo "You need to set the S3_SECRET_ACCESS_KEY environment variable."
+  exit 1
+fi
+
+if [ "${S3_BUCKET}" = "**None**" ]; then
+  echo "You need to set the S3_BUCKET environment variable."
+  exit 1
+fi
+
+if [ "${POSTGRES_DATABASE}" = "**None**" -a "${POSTGRES_BACKUP_ALL}" != "true" ]; then
+  echo "You need to set the POSTGRES_DATABASE environment variable."
+  exit 1
+fi
+
+if [ "${POSTGRES_HOST}" = "**None**" ]; then
+  if [ -n "${POSTGRES_PORT_5432_TCP_ADDR}" ]; then
+    POSTGRES_HOST=$POSTGRES_PORT_5432_TCP_ADDR
+    POSTGRES_PORT=$POSTGRES_PORT_5432_TCP_PORT
+  else
+    echo "You need to set the POSTGRES_HOST environment variable."
+    exit 1
+  fi
+fi
+
+if [ "${POSTGRES_USER}" = "**None**" ]; then
+  echo "You need to set the POSTGRES_USER environment variable."
+  exit 1
+fi
+
+if [ "${POSTGRES_PASSWORD}" = "**None**" ]; then
+  echo "You need to set the POSTGRES_PASSWORD environment variable or link to a container named POSTGRES."
+  exit 1
+fi
+
+if [ -z ${POSTGRES_VERSION+x} ]; then
+  POSTGRES_VERSION=""
+else
+  POSTGRES_VERSION="${POSTGRES_VERSION}/"
+fi
+
+if [ "${S3_ENDPOINT}" == "**None**" ]; then
+  AWS_ARGS=""
+else
+  AWS_ARGS="--endpoint-url ${S3_ENDPOINT}"
+fi
+
+# env vars needed for aws tools
+export AWS_ACCESS_KEY_ID=$S3_ACCESS_KEY_ID
+export AWS_SECRET_ACCESS_KEY=$S3_SECRET_ACCESS_KEY
+export AWS_DEFAULT_REGION=$S3_REGION
+
+export PGPASSWORD=$POSTGRES_PASSWORD
+POSTGRES_HOST_OPTS="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER $POSTGRES_EXTRA_OPTS"
+
+if [ -z ${S3_PREFIX+x} ]; then
+  S3_PREFIX="/"
+else
+  S3_PREFIX="/${S3_PREFIX}/"
+fi
+
+if [ "${POSTGRES_BACKUP_ALL}" == "true" ]; then
+  DB_LIST=$(psql $POSTGRES_HOST_OPTS -t -c "SELECT datname FROM pg_database WHERE datistemplate = false AND datname != 'default';")
+
+  for DB in $DB_LIST; do
+    SRC_FILE=dump.sql.gz
+    DEST_FILE=${DB}_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz
+
+    echo "Creating dump of ${DB} database from ${POSTGRES_HOST}..."
+    if ! pg_dump $POSTGRES_HOST_OPTS "$DB" | gzip > "$SRC_FILE"; then
+      >&2 echo "Error creating dump for database: ${DB}"
+      continue
+    fi
+
+    if [ "${ENCRYPTION_PASSWORD}" != "**None**" ] && [ -n "${ENCRYPTION_PASSWORD}" ]; then
+      echo "Encrypting ${SRC_FILE}"
+      if ! openssl enc -aes-256-cbc -in "$SRC_FILE" -out "${SRC_FILE}.enc" -k "$ENCRYPTION_PASSWORD"; then
+        >&2 echo "Error encrypting ${SRC_FILE}"
+        continue
+      fi
+      rm "$SRC_FILE"
+      SRC_FILE="${SRC_FILE}.enc"
+      DEST_FILE="${DEST_FILE}.enc"
+    fi
+
+    echo "Uploading dump to $S3_BUCKET"
+    echo "Uploaded to s3://${S3_BUCKET}${S3_PREFIX}${POSTGRES_VERSION}${DEST_FILE}"
+    cat $SRC_FILE | aws $AWS_ARGS s3 cp - "s3://${S3_BUCKET}${S3_PREFIX}${POSTGRES_VERSION}${DEST_FILE}" || exit 2
+
+
+    echo "SQL backup uploaded successfully"
+    rm -rf $SRC_FILE
+  done
+else
+  OIFS="$IFS"
+  IFS=','
+  for DB in $POSTGRES_DATABASE
+  do
+    IFS="$OIFS"
+
+    SRC_FILE=dump.sql.gz
+    DEST_FILE=${DB}_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz
+
+    echo "Creating dump of ${DB} database from ${POSTGRES_HOST}..."
+    pg_dump $POSTGRES_HOST_OPTS $DB | gzip > $SRC_FILE
+
+    if [ "${ENCRYPTION_PASSWORD}" != "**None**" ]; then
+      echo "Encrypting ${SRC_FILE}"
+      openssl enc -aes-256-cbc -in $SRC_FILE -out ${SRC_FILE}.enc -k $ENCRYPTION_PASSWORD
+      if [ $? != 0 ]; then
+        >&2 echo "Error encrypting ${SRC_FILE}"
+      fi
+      rm $SRC_FILE
+      SRC_FILE="${SRC_FILE}.enc"
+      DEST_FILE="${DEST_FILE}.enc"
+    fi
+
+    echo "Uploading dump to $S3_BUCKET"
+    cat $SRC_FILE | aws $AWS_ARGS s3 cp - "s3://${S3_BUCKET}${S3_PREFIX}${DEST_FILE}" || exit 2
+
+    echo "SQL backup uploaded successfully"
+    rm -rf $SRC_FILE
+  done
+fi
---
+++ postgres-17-backup-s3/README.md
@@ -0,0 +1,62 @@
+# postgres-backup-s3
+
+Backup PostgresSQL to S3 (supports periodic backups)
+
+## Usage
+
+Docker:
+```sh
+$ docker run -e S3_ACCESS_KEY_ID=key -e S3_SECRET_ACCESS_KEY=secret -e S3_BUCKET=my-bucket -e S3_PREFIX=backup -e POSTGRES_DATABASE=dbname -e POSTGRES_USER=user -e POSTGRES_PASSWORD=password -e POSTGRES_HOST=localhost dzangolab/postgres-backup-s3
+```
+
+Docker Compose:
+```yaml
+postgres:
+  image: postgres
+  environment:
+    POSTGRES_USER: user
+    POSTGRES_PASSWORD: password
+
+pgbackups3:
+  image: dzangolab/postgres-backup-s3
+  links:
+    - postgres
+  environment:
+    SCHEDULE: '@daily'
+    S3_REGION: region
+    S3_ACCESS_KEY_ID: key
+    S3_SECRET_ACCESS_KEY: secret
+    S3_BUCKET: my-bucket
+    S3_PREFIX: backup
+    POSTGRES_DATABASES: dbname
+    POSTGRES_USER: user
+    POSTGRES_PASSWORD: password
+    POSTGRES_EXTRA_OPTS: '--schema=public --blobs'
+```
+## Environment variables
+
+- `POSTGRES_EXTRA_OPTS` pg_dump options (default: '')
+- `POSTGRES_DATABASES` list of databases you want to backup *required*
+- `POSTGRES_HOST` the postgres host *required*
+- `POSTGRES_PORT` the postgres port (default: 5432)
+- `POSTGRES_USER` the postgres user *required*
+- `POSTGRES_PASSWORD` the mysql password *required*
+- `POSTGRES_PASSWORD_FILE` path to file containing the postgres password; alternative to `POSTGRES_PASSWORD`
+- `S3_ACCESS_KEY_ID` your AWS access key *required*
+- `S3_ACCESS_KEY_ID_FILE` path to file containing your AWS access key; alternative to `S3_ACCESS_KEY_ID`
+- `S3_SECRET_ACCESS_KEY` your AWS secret key *required*
+- `S3_SECRET_ACCESS_KEY_FILE` path to file containing  your AWS secret key; alternative to `S3_SECRET_ACCESS_KEYs`
+- `S3_BUCKET` your AWS S3 bucket path *required*
+- `S3_PREFIX` path prefix in your bucket (default: 'backup')
+- `S3_FILENAME` a consistent filename to overwrite with your backup.  If not set will use a timestamp.
+- `S3_REGION` the AWS S3 bucket region (default: us-west-1)
+- `S3_ENDPOINT` the AWS Endpoint URL, for S3 Compliant APIs such as [minio](https://minio.io) (default: none)
+- `S3_S3V4` set to `yes` to enable AWS Signature Version 4, required for [minio](https://minio.io) servers (default: no)
+- `SCHEDULE` backup schedule time, see explainatons below
+
+### Automatic Periodic Backups
+
+You can additionally set the `SCHEDULE` environment variable like `-e SCHEDULE="@daily"` to run the backup automatically.
+
+More information about the scheduling can be found [here](http://godoc.org/github.com/robfig/cron#hdr-Predefined_schedules).
+
