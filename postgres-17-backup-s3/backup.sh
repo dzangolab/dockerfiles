4664ed9479646426ac39664c79c8e415d008b654
Olivier Pichon <op@dzango.com>
2025-09-06 22:21:50 +0700

    fix(postgres-17-s3-backup): fix image

--- postgres-17-backup-s3/backup.sh
+++ postgres-17-backup-s3/backup.sh
@@ -1,46 +1,21 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -x
 set -eo pipefail
 
 echo "Starting backup at $(date)"
 
-if [ -n "${POSTGRES_PASSWORD_FILE}" ]; then
-  POSTGRES_PASSWORD=$(cat "$POSTGRES_PASSWORD_FILE")
-  export POSTGRES_PASSWORD
-fi
-
-if [ -n "${S3_ACCESS_KEY_ID_FILE}" ]; then
-  S3_ACCESS_KEY_ID=$(cat "$S3_ACCESS_KEY_ID_FILE")
-  export S3_ACCESS_KEY_ID
-fi
-
-if [ -n "${S3_SECRET_ACCESS_KEY_FILE}" ]; then
-  S3_SECRET_ACCESS_KEY=$(cat "$S3_SECRET_ACCESS_KEY_FILE")
-  export S3_SECRET_ACCESS_KEY
-fi
-
-if [ "${S3_ACCESS_KEY_ID}" = "**None**" ]; then
-  echo "You need to set the S3_ACCESS_KEY_ID environment variable."
-  exit 1
-fi
-
-if [ "${S3_SECRET_ACCESS_KEY}" = "**None**" ]; then
-  echo "You need to set the S3_SECRET_ACCESS_KEY environment variable."
-  exit 1
-fi
-
-if [ "${S3_BUCKET}" = "**None**" ]; then
+if [ -z "${S3_BUCKET}" ]; then
   echo "You need to set the S3_BUCKET environment variable."
   exit 1
 fi
 
-if [ "${POSTGRES_DATABASE}" = "**None**" -a "${POSTGRES_BACKUP_ALL}" != "true" ]; then
-  echo "You need to set the POSTGRES_DATABASE environment variable."
+if [ -z "${POSTGRES_DATABASES}" ]; then
+  echo "You need to set the POSTGRES_DATABASES environment variable."
   exit 1
 fi
 
-if [ "${POSTGRES_HOST}" = "**None**" ]; then
+if [ -z "${POSTGRES_HOST}" ]; then
   if [ -n "${POSTGRES_PORT_5432_TCP_ADDR}" ]; then
     POSTGRES_HOST=$POSTGRES_PORT_5432_TCP_ADDR
     POSTGRES_PORT=$POSTGRES_PORT_5432_TCP_PORT
@@ -50,23 +25,23 @@ if [ "${POSTGRES_HOST}" = "**None**" ]; then
   fi
 fi
 
-if [ "${POSTGRES_USER}" = "**None**" ]; then
+if [ -z "${POSTGRES_USER}" ]; then
   echo "You need to set the POSTGRES_USER environment variable."
   exit 1
 fi
 
-if [ "${POSTGRES_PASSWORD}" = "**None**" ]; then
+if [ -z "${POSTGRES_PASSWORD}" ]; then
   echo "You need to set the POSTGRES_PASSWORD environment variable or link to a container named POSTGRES."
   exit 1
 fi
 
-if [ -z ${POSTGRES_VERSION+x} ]; then
+if [ -z "${POSTGRES_VERSION}" ]; then
   POSTGRES_VERSION=""
 else
-  POSTGRES_VERSION="${POSTGRES_VERSION}/"
+  POSTGRES_VERSION=".${POSTGRES_VERSION}"
 fi
 
-if [ "${S3_ENDPOINT}" == "**None**" ]; then
+if [ -z "${S3_ENDPOINT}" ]; then
   AWS_ARGS=""
 else
   AWS_ARGS="--endpoint-url ${S3_ENDPOINT}"
@@ -80,78 +55,49 @@ export AWS_DEFAULT_REGION=$S3_REGION
 export PGPASSWORD=$POSTGRES_PASSWORD
 POSTGRES_HOST_OPTS="-h $POSTGRES_HOST -p $POSTGRES_PORT -U $POSTGRES_USER $POSTGRES_EXTRA_OPTS"
 
-if [ -z ${S3_PREFIX+x} ]; then
+if [ -z "${S3_PREFIX}" ]; then
   S3_PREFIX="/"
 else
   S3_PREFIX="/${S3_PREFIX}/"
 fi
 
-if [ -z ${S3_SUFFIX+x} ]; then
+if [ -z "${S3_SUFFIX}" ]; then
   S3_SUFFIX=""
 else
   S3_SUFFIX="-${S3_SUFFIX}"
 fi
 
-if [ "${POSTGRES_BACKUP_ALL}" == "true" ]; then
+if [ "${POSTGRES_DATABASES}" = "ALL" ]; then
   DB_LIST=$(psql $POSTGRES_HOST_OPTS -t -c "SELECT datname FROM pg_database WHERE datistemplate = false AND datname != 'default';")
+else 
+  DB_LIST = "${POSTGRES_DATABASES//,/ }"
+fi
 
-  for DB in $DB_LIST; do
-    SRC_FILE=dump.sql.gz
-    DEST_FILE=${DB}${S3_SUFFIX}_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz
+for DB in $DB_LIST; do
+  SRC_FILE=dump.sql.gz
+  DEST_FILE=${DB}/$(date +"%Y")/$(date +"%m")/${DB}${POSTGRES_VERSION}.$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz
 
-    echo "Creating dump of ${DB} database from ${POSTGRES_HOST}..."
-    if ! pg_dump $POSTGRES_HOST_OPTS "$DB" | gzip > "$SRC_FILE"; then
-      >&2 echo "Error creating dump for database: ${DB}"
+  echo "Creating dump of ${DB} database from ${POSTGRES_HOST}..."
+  if ! pg_dump $POSTGRES_HOST_OPTS "$DB" | gzip > "$SRC_FILE"; then
+    >&2 echo "Error creating dump for database: ${DB}"
+    continue
+  fi
+
+  if [ -n "${ENCRYPTION_PASSWORD}" ]; then
+    echo "Encrypting ${SRC_FILE}"
+    if ! openssl enc -aes-256-cbc -in "$SRC_FILE" -out "${SRC_FILE}.enc" -k "$ENCRYPTION_PASSWORD"; then
+      >&2 echo "Error encrypting ${SRC_FILE}"
       continue
     fi
+    rm "$SRC_FILE"
+    SRC_FILE="${SRC_FILE}.enc"
+    DEST_FILE="${DEST_FILE}.enc"
+  fi
 
-    if [ "${ENCRYPTION_PASSWORD}" != "**None**" ] && [ -n "${ENCRYPTION_PASSWORD}" ]; then
-      echo "Encrypting ${SRC_FILE}"
-      if ! openssl enc -aes-256-cbc -in "$SRC_FILE" -out "${SRC_FILE}.enc" -k "$ENCRYPTION_PASSWORD"; then
-        >&2 echo "Error encrypting ${SRC_FILE}"
-        continue
-      fi
-      rm "$SRC_FILE"
-      SRC_FILE="${SRC_FILE}.enc"
-      DEST_FILE="${DEST_FILE}.enc"
-    fi
+  echo "Uploading dump to $S3_BUCKET"
+  cat $SRC_FILE | aws $AWS_ARGS s3 cp - "s3://${S3_BUCKET}${S3_PREFIX}${DEST_FILE}" || exit 2
 
-    echo "Uploading dump to $S3_BUCKET"
-    echo "Uploaded to s3://${S3_BUCKET}${S3_PREFIX}${POSTGRES_VERSION}${DEST_FILE}"
-    cat $SRC_FILE | aws $AWS_ARGS s3 cp - "s3://${S3_BUCKET}${S3_PREFIX}${POSTGRES_VERSION}${DEST_FILE}" || exit 2
-
-
-    echo "SQL backup uploaded successfully"
-    rm -rf $SRC_FILE
-  done
-else
-  OIFS="$IFS"
-  IFS=','
-  for DB in $POSTGRES_DATABASE
-  do
-    IFS="$OIFS"
-
-    SRC_FILE=dump.sql.gz
-    DEST_FILE=${DB}${S3_SUFFIX}_$(date +"%Y-%m-%dT%H:%M:%SZ").sql.gz
-
-    echo "Creating dump of ${DB} database from ${POSTGRES_HOST}..."
-    pg_dump $POSTGRES_HOST_OPTS $DB | gzip > $SRC_FILE
-
-    if [ "${ENCRYPTION_PASSWORD}" != "**None**" ]; then
-      echo "Encrypting ${SRC_FILE}"
-      openssl enc -aes-256-cbc -in $SRC_FILE -out ${SRC_FILE}.enc -k $ENCRYPTION_PASSWORD
-      if [ $? != 0 ]; then
-        >&2 echo "Error encrypting ${SRC_FILE}"
-      fi
-      rm $SRC_FILE
-      SRC_FILE="${SRC_FILE}.enc"
-      DEST_FILE="${DEST_FILE}.enc"
-    fi
-
-    echo "Uploading dump to $S3_BUCKET"
-    cat $SRC_FILE | aws $AWS_ARGS s3 cp - "s3://${S3_BUCKET}${S3_PREFIX}${DEST_FILE}" || exit 2
-
-    echo "SQL backup uploaded successfully"
-    rm -rf $SRC_FILE
-  done
-fi
+  echo "SQL backup uploaded successfully"
+  echo "Uploaded to s3://${S3_BUCKET}${S3_PREFIX}${DEST_FILE}"
+  rm -rf $SRC_FILE
+done
--- postgres-17-backup-s3/run.sh
+++ postgres-17-backup-s3/run.sh
@@ -1,13 +1,17 @@
-#! /bin/bash
+#!/usr/env/bin bash
 
 set -e
+
+. /expand_secrets.sh
+
+expand_secrets
 
 if [ "${S3_S3V4}" = "yes" ]; then
     aws configure set default.s3.signature_version s3v4
 fi
 
-if [ "${SCHEDULE}" = "**None**" ]; then
-  sh backup.sh
+if [ -z "${SCHEDULE}" ]; then
+  /usr/bin/env bash backup.sh
 else
-  exec go-cron "$SCHEDULE" /bin/bash backup.sh
+  exec go-cron "$SCHEDULE" /usr/bin/env bash backup.sh
 fi
