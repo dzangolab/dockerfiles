e4a7696f919b69f44aa018feb940825e00a5c165
Olivier Pichon <op@dzango.com>
2025-09-12 10:08:38 +0200

    feat(nodocb): add support for generating the NC_DB url

--- nocodb/start.sh
+++ nocodb/start.sh
@@ -4,4 +4,25 @@
 
 expand_secrets
 
+# If NC_DB is not set, construct it from individual components
+if [ -z "${NC_DB:-}" ]; then
+  # URL encode the password if it's provided in plain text
+  if [ -n "${DATABASE_PASSWORD:-}" ]; then
+    # Function to URL encode the password
+    urlencode() {
+      node -p "encodeURIComponent('${1//\'/\\\'}')"
+    }
+
+    encoded_password=$(urlencode "$DATABASE_PASSWORD")
+
+    if [ -n "${DATABASAE_PORT:-}" ]; then
+      DATABASE_PORT=":${DATABASE_PORT}"
+    fi
+
+    NC_DB="pg://${DATABASE_HOST}${DATABASE_PORT}?u=${DATABASE_USER}&p=${encoded_password}&d=${DATABASE_NAME}"
+  fi
+
+  export NC_DB
+fi
+
 bash /usr/src/appEntry/start.sh
