470a5e6ceac452be1ef239fafb08311eb28b32d4
Olvier Pichon <op@dzango.com>
2026-06-21 08:30:05 +0700

    fix(build): fix build script

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash -eux
 
+if ! docker buildx inspect multiplatform &>/dev/null; then
+  docker buildx create --name multiplatform --driver docker-container --bootstrap
+fi
+
+docker buildx use multiplatform
+
 cd $1
 docker buildx build --platform linux/amd64,linux/arm64 -t dzangolab/$1:$2 --push .
